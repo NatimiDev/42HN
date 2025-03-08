74278a2e2821dafb15581ba039911420dbb46e16
Natalia Mikuka <nmikuka@1-C-18.42heilbronn.de>
2025-03-08 12:38:13 +0100

    read and write only 1 symbol, otherwise moulinette binary files check fails

--- piscine_reloaded/ex27/main.c
+++ piscine_reloaded/ex27/main.c
@@ -13,32 +13,17 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-void	ft_putstr(char *str, int out)
-{
-	while (*str != '\0')
-	{
-		write(out, str, 1);
-		str++;
-	}
-}
-
 int	print_file_content(char *filename)
 {
 	int		fd;
-	int		buffer_size;
-	int		bytes_read; 
-	char	content[1<<6];
+	char	content;
 
-	buffer_size = 1<<6;
-	bytes_read = buffer_size;
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
 		return (0);
-	while (bytes_read > 0)
+	while (read(fd, &content, 1))
 	{
-		bytes_read = read(fd, content, buffer_size - 1);
-		content[bytes_read] = '\0';
-		ft_putstr(content, 1);
+		write(1, &content, 1);
 	}
 	close(fd);
 	return (1);
@@ -48,17 +33,17 @@ int	main(int argc, char *argv[])
 {
 	if (argc < 2)
 	{
-		ft_putstr("File name missing.\n", 2);
+		write(2, "File name missing.\n", 19);
 		return (1);
 	}
 	if (argc > 2)
 	{
-		ft_putstr("Too many arguments.\n", 2);
+		write(2, "Too many arguments.\n", 20);
 		return (1);
 	}
 	if (!print_file_content(argv[1]))
 	{
-		ft_putstr("Cannot read file.\n", 2);
+		write(2, "Cannot read file.\n", 18);
 		return (1);
 	}
 	return (0);
