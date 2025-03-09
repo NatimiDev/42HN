9d6f2e5160315f47a8410c788c7f72bd01552898
NatimiDev <nati.mikula@gmail.com>
2025-03-09 17:32:52 +0100

    fix norminette issues

--- libft/ft_putendl_fd.c
+++ libft/ft_putendl_fd.c
@@ -6,14 +6,14 @@
 /*   By: nmikuka <nmikuka@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/09 15:20:11 by nmikuka           #+#    #+#             */
-/*   Updated: 2025/03/09 15:28:17 by nmikuka          ###   ########.fr       */
+/*   Updated: 2025/03/09 17:29:24 by nmikuka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_putendl_fd(char *s, int fd)
-{	
+{
 	if (!s)
 		return ;
 	ft_putstr_fd(s, fd);
